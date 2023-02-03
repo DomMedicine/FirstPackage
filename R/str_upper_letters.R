1a15728b63e53f89e7ea9cd80d341977eaf27a76
DomMedicine <nowakowskisf@gmail.com>
2023-02-03 20:37:37 +0100

    2nd version of package

--- R/str_upper_letters.R
+++ R/str_upper_letters.R
@@ -1,14 +1,15 @@
-#' Title
+#' convert the word from lower to upper letters
 #'
 #' @param napis napis ktory chcemy zrobic wielkimi literami
-#' @inheritParams stringr::str_to_upper
 #'
 #' @return napis zapisany wielkimi literami
 #' @export
 #'
 #' @examples
-str_upper_letters <- function(napis){
-  if(grepl("[0-9]|[A-Z]|[[:space:]]", napis)){ # to dziala ale nie dziala na znaki specjalne polskie
+#' x <- 'example'
+#' str_upper_letters(x)
+str_upper_letters <- function(napis){ # [ĄąŻżŚśŹźĘęĆćŃńÓóŁł]
+  if(grepl("[[:upper:]]|[[:digit:]]|[[:space:]]|[[:blank:]]|[[:punct:]]", napis)){
     stop("zawiera nieprawdziwe dane!")
   }
   stringr::str_to_upper(napis)
--- tests/testthat/test-str_upper_letters.R
+++ tests/testthat/test-str_upper_letters.R
@@ -6,10 +6,18 @@ test_that('number', {
   expect_error(str_upper_letters("an1a"))
 })
 
-test_that('duze litery', {
+test_that('upper letters', {
   expect_error(str_upper_letters("Ana"))
 })
 
 test_that('correct', {
   expect_equal(str_upper_letters("anna"), "ANNA")
+})
+
+# test_that('polskie litery', {
+#   expect_error(str_upper_letters("anną"))
+# })
+
+test_that('special marks', {
+  expect_error(str_upper_letters("anna["))
 })
